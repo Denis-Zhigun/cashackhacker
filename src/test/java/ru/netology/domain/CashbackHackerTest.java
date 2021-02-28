a94a74d2f3f9f479e69b7575ad4bce8d179acde6
Denis Zhigun <zhigundv@gmail.com>
2021-02-28 14:06:40 +0300

    Modifed testNG

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -1,8 +1,8 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 class CashbackHackerTest {
     public CashbackHacker cashbackHacker = new CashbackHacker();
